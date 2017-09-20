8422dcb137dc13ecf83a1622e631f5f5b98187ce
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-09-20 18:02:49 -0500

    Agregar ejemplo de conexión remota a un servidor

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,42 @@ import { Http } from './utils/http';
 
 let question = new TestQuestion( 1, "Pregunta #1", [], "Repuesta" );
 let test = new Test( [] );
-console.log( test, question );
+
+let serviceURL = 'http://projects-api.webtraining.zone:4000/traffic-signals/v1/signals';
+
+
+export class App {
+
+	constructor() {
+		this.signals = [];
+	}
+
+	static getTrafficSignalsData() {
+		return new Promise( ( resolve, reject ) => {
+			Http.get( serviceURL ).then( ( response ) => {
+				resolve( response.data );
+			} );
+		} );
+	}
+
+	static renderTrafficSignalsCatalog( selector, data ) {
+		let template = `{{#each signals}}`;
+		let container = document.querySelector( selector );
+		container.innerHTML = template;
+
+		console.log( "Rendering with", container, data );
+	}
+
+	init() {
+
+		App.getTrafficSignalsData()
+			.then( ( data ) => {
+				const selector = '#app';
+				App.renderTrafficSignalsCatalog( selector, data );
+			} );
+	}
+}
+
+const app = new App();
+app.init();
+
--- src/js/utils/http.js
+++ src/js/utils/http.js
@@ -1,14 +1,26 @@
-import * as axios from 'axios';
+import axios from 'axios/dist/axios';
 
 export class Http {
-	// static get( url, headers ) {
-	//
-	// 	const config = {
-	// 		headers: headers,
-	// 		data: data,
-	// 		method: 'get'
-	// 	};
-	//
-	// 	return axios.request( url, config );
-	// }
+	static request( url, config = { method: 'get', data: {}, headers: {} } ) {
+		const finalConfig = Object.assign( config );
+
+		return axios.request( url, finalConfig );
+	}
+
+	static get( url, config ) {
+		return this.request( url, config );
+
+	}
+
+	static post( url, config ) {
+		return this.request( url, Object.assign( { method: 'post', config } ) );
+	}
+
+	static put( url, config ) {
+		return this.request( url, Object.assign( { method: 'put', config } ) );
+	}
+
+	static deleteRequest( url, config ) {
+		return this.request( url, Object.assign( { method: 'delete', config } ) );
+	}
 }
