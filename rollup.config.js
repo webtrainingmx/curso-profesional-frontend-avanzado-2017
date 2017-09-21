82d219891ae8fd262a33b133a02068111d0aea74
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-09-21 14:25:23 -0500

    Agregar paquetes para soporte de templates de Handlebars

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import handlebars from 'rollup-plugin-handlebars-plus';
+// import rootImport from 'rollup-plugin-root-import';
+// let partialRoots = [ `${__dirname}/src/client/js/views/`, `${__dirname}/src/common/views/` ];
 
 export default {
 	input: './src/js/app.js',
@@ -19,10 +22,17 @@ export default {
 			}
 		} ),
 		babel( {
-			exclude: 'node_modules/**' // only transpile our source code
+			exclude: [ 'node_modules/**', 'src/js/components/**/templates/*.hbs' ] // only transpile our source code
 		} ),
 		commonjs( {
-			// include: 'node_modules/**'
+			include: 'node_modules/**'
+		} ),
+		// Required by use of `partialRoot` below.
+		// rootImport( {
+		// 	root: partialRoots
+		// } ),
+		handlebars( {
+			// helpers: [ '/src/js/render-engine/helpers.js' ]
 		} )
 	],
 	watch: {
