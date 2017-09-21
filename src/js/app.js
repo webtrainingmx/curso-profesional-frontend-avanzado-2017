92ffee690ec4dfdbed4b6be97c6f747ea71921d5
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-09-21 14:25:56 -0500

    Agregar ejemplo de Handlebars template

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import { Test } from './components/test/test';
 import { TestQuestion } from './components/test/models/test-question.model';
 import { Http } from './utils/http';
+import { Renderify } from './render-engine/renderify';
+import TrafficSignalsCatalogTemplate from './components/test/templates/traffic-signals-catalog.hbs';
+
 
 let question = new TestQuestion( 1, "Pregunta #1", [], "Repuesta" );
 let test = new Test( [] );
@@ -11,23 +14,25 @@ let serviceURL = 'http://projects-api.webtraining.zone:4000/traffic-signals/v1/s
 export class App {
 
 	constructor() {
-		this.signals = [];
+
 	}
 
 	static getTrafficSignalsData() {
 		return new Promise( ( resolve, reject ) => {
 			Http.get( serviceURL ).then( ( response ) => {
+				let widget = new Renderify( {
+					selector: '#app',
+					compiledTemplate: TrafficSignalsCatalogTemplate,
+					data: response.data
+				} );
+				widget.render();
 				resolve( response.data );
 			} );
 		} );
 	}
 
 	static renderTrafficSignalsCatalog( selector, data ) {
-		let template = `{{#each signals}}`;
-		let container = document.querySelector( selector );
-		container.innerHTML = template;
 
-		console.log( "Rendering with", container, data );
 	}
 
 	init() {
